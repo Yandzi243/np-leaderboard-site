9b32a6ae5c937fd6d268628fd128be016e319bed
Yandzi243 <yandzisila@gmail.com>
2026-05-31 23:53:59 +0300

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBs10LVUQTKtvBt3im0W5Pp_XgKqmnmU-w",
+  authDomain: "night-prowlers.firebaseapp.com",
+  projectId: "night-prowlers",
+  storageBucket: "night-prowlers.firebasestorage.app",
+  messagingSenderId: "192565490320",
+  appId: "1:192565490320:web:99b9a3c3efce481c689e15"
+};
+
+const app = initializeApp(firebaseConfig);
